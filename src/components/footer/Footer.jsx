40ecbc4d02d4f86d5f45e609a175c6bc99ec2704
Akash <chowdhuryakash950@gmail.com>
2023-04-14 02:07:59 +0530

    Moviz app clone completed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <div className="infoText">
+                <strong>&copy; 2023 Created by Akash Chowdhury.  </strong>
+     All rights reserved.
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
